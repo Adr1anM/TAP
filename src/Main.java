281d6c0cc03a5e30cfc64aa6786f7d969157972e
Adrian <andrian.melnic2003@gmail.com>
2024-02-21 08:37:43 +0200

    Finished lab5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(2);
+        numbers.add(2);
 
+
+
+
+        try {
+            int step = 1;
+            if (numbers.size() <= 1) {
+                throw new IndexOutOfBoundsException();
+            }
+            while (numbers.size() > 1) {
+                ArrayList<Integer> sums = new ArrayList<>();
+                int i = 0;
+                while (i < numbers.size()) {
+                    try {
+                        int sum = numbers.get(i) + numbers.get(i + 1);
+                        sums.add(sum);
+                        i += 2;
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(e.getMessage());
+                        sums.add(numbers.get(i));
+                        i++;
+                    }
+                }
+                numbers = sums;
+                System.out.println("Etapa " + step++ + ": " + numbers);
+            }
+            System.out.println("Suma perechilor: " + numbers.getFirst());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Nu există numere pentru sumare.");
+        }
     }
 }
